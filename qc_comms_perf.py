25d83f9449d9c2dbd232523cc9ab614f6a6e5a1f
Kristian Ivanov <71103348+kivanov10@users.noreply.github.com>
2022-05-13 06:11:02 +0100

    Add files via upload

--- qc_comms_perf.py
+++ qc_comms_perf.py
@@ -1,11 +1,12 @@
 import qiskit as q
-from qiskit.visualization import plot_histogram, plot_bloch_multivector, array_to_latex
+from qiskit.visualization import plot_histogram, plot_bloch_multivector
 from qiskit.extensions import Initialize
 from qiskit.quantum_info import random_statevector
+from qiskit.result import marginal_counts
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#tamper with the wavefunction on purpouse
 class qc_comms:
     def __init__(self):
         self.random_state()
@@ -29,16 +30,32 @@ class qc_comms:
         self.receiver_gates(self.qc, 2, self.bits_z, self.bits_x)
 
         self.qc.draw('mpl')
+        # plt.show()
         self.sim = q.Aer.get_backend('aer_simulator')
         self.qc.save_statevector()
         out_vector = self.sim.run(self.qc).result().get_statevector()
         plot_bloch_multivector(out_vector)
+        # plt.show()
+        # Error is here, disentagler is not recognised by line 47
+        # self.qc.append(self.init_gate_inv, [2])
+        self.qc.measure(2,2)
+        self.qc.draw('mpl')
+        # print(self.qc)
+        # big_qc = q.transpile(self.qc, self.sim)
+        # big_qc.save_statevector()
+        # print(big_qc)
+        # results = self.sim.run(self.qc).result().get_counts()
+        # qubit_counts = [marginal_counts(results, [qub]) for qub in range(3)]
+        # plot_histogram(qubit_counts)
+        print('lol')
         plt.show()
 
+
     def random_state(self):
-        """Creates a random statevector"""
+        """Creates a random statevector it's inverse"""
         self.psi = random_statevector(2)
         self.init_gate = Initialize(self.psi)
+        self.init_gate_inv = self.init_gate.gates_to_uncompute()
         self.init_gate.label = "Random state"
 
 
@@ -47,7 +64,8 @@ class qc_comms:
         qubits = q.QuantumRegister(3, name='qbit')
         self.bits_z = q.ClassicalRegister(1, name='bit_z')
         self.bits_x = q.ClassicalRegister(1, name='bit_x')
-        self.qc = q.QuantumCircuit(qubits, self.bits_z, self.bits_x)
+        self.bits_r = q.ClassicalRegister(1, name='bit_rec')
+        self.qc = q.QuantumCircuit(qubits, self.bits_z, self.bits_x, self.bits_r)
 
     def bell_pair(self, qc, q1, q2):
         """Creates a bell pair with q1 being control. These qubits are then "sent"
